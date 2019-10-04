913bf149e8c66885c5a0d6785af44b8f3b66232f
Leandro Fogaça <fogaca.lf@gmail.com>
2019-10-03 22:17:44 -0300

    buzzer
    inserido o código do buzzer

--- Arduino_2/Arduino_2.ino
+++ Arduino_2/Arduino_2.ino
@@ -9,7 +9,7 @@
 #define pinled3 A2
 #define pinled4 A3
 #define pinled5 A4
-#define pinled6 A5
+#define pinled6 A5 //Led Vermelho
 
 #define Buzzer 6
 
@@ -23,7 +23,7 @@
 
 LiquidCrystal lcd(RS, E, db4, db5, db6, db7); //Configuração do LCD
 
-int Rx, RxF=0;  //auxiliar e Flag do RX
+int Rx, RxF = 0; //auxiliar e Flag do RX
 char RxBuff[8]; // Menssagem que será recebida
 
 
@@ -32,12 +32,23 @@ Servo servoJnl;
 Servo servoPt1;
 Servo servoPt2;
 
+bool Chv1;
+bool Chv2;
+bool Chv3;
+bool Tag1;
+bool Tag2;
+bool AlarmeAcionado;
+bool AcionamentoPortao;
 
+String myString;
+int cmd;
 
+int tempo = 200;
+void blink(int var, int time);
 
 void setup()
 {
-  
+
   lcd.begin(16, 2);
   Serial.begin(9600);
   pinMode(pinled1 , OUTPUT);
@@ -46,17 +57,18 @@ void setup()
   pinMode(pinled4 , OUTPUT);
   pinMode(pinled5 , OUTPUT);
   pinMode(pinled6 , OUTPUT);
-  
+
   pinMode(Buzzer, OUTPUT);
- 
-  
+
+
   servoTdo.attach(5);  //anexa portão do telhado na porta 5
   servoJnl.attach(9);  //anexa portão da Janela na porta 9
   servoPt1.attach(10); //anexa portão do Portão1 na porta 10
   servoPt2.attach(11); //anexa portão do Portão2 na porta 11
-  
-  
-  
+
+
+
+
   lcd.clear();
   delay(200);
   lcd.setCursor(3, 0);
@@ -66,41 +78,96 @@ void setup()
 
 void loop()
 {
- 
 
-      if (Serial.available() > 0)
-         {
-            Rx = Bluetooth.read();
-            RxBuff[8]=RxBuff[7];
-            RxBuff[7]=RxBuff[6];
-            RxBuff[6]=RxBuff[5];
-            RxBuff[5]=RxBuff[4];
-            RxBuff[4]=RxBuff[3];
-            RxBuff[3]=RxBuff[2];
-            RxBuff[2]=RxBuff[1];
-            RxBuff[1]=RxBuff[0];
-            RxBuff[0]=Rx;
-      
-           RxF = 1; //Flag para sinalizar que a informação é nova
-         }
-  
-  
-       if ((RxBuff[8] == '!') && (RxBuff[0]=='#') && (RxF==1))
-          {             
-               for (int i = 7; i >= 4; --i) {   
-                String myString.concat(RxBuff[i]);     
-               }
-                 
-               int cmd = (RxBuff[3]-48)*100)+(RxBuff[2]-48)*10)+(RxBuff[1]-48)*1)       
+
+  if (Serial.available() > 0)
+  {
+    Rx = Serial.read();
+    RxBuff[8] = RxBuff[7];
+    RxBuff[7] = RxBuff[6];
+    RxBuff[6] = RxBuff[5];
+    RxBuff[5] = RxBuff[4];
+    RxBuff[4] = RxBuff[3];
+    RxBuff[3] = RxBuff[2];
+    RxBuff[2] = RxBuff[1];
+    RxBuff[1] = RxBuff[0];
+    RxBuff[0] = Rx;
+
+    RxF = 1; //Flag para sinalizar que a informação é nova
+  }
+
+
+  if ((RxBuff[8] == '!') && (RxBuff[0] == '#') && (RxF == 1))
+  {
+    for (int i = 7; i >= 4; --i) {
+      myString.concat(RxBuff[i]);
+    }
+
+    cmd = ((RxBuff[3] - 48) * 100) + ((RxBuff[2] - 48) * 10) + ((RxBuff[1] - 48) * 1);
+
+
+    //Decidir oque fazer com a variavel
+
+    RxF = 0;
+  }
+
+
+
+  if (((Chv1 == HIGH) || (Chv2 == HIGH) || (Chv3 == HIGH)) && (Tag1 == LOW)
+      && (Tag1 == LOW) && (AlarmeAcionado == HIGH))
+  {
+    tone(Buzzer, 1500, 500);
     
-    
-          //Decidir oque fazer com a variavel
-    
-    
-               RxF = 0;
-            }
-    
-  
-  
-  
+    blink(pinled6, tempo);
+  }
+  if (AlarmeAcionado == LOW) {
+    noTone(Buzzer);
+  }
+
+
+ /*
+
+  if (AcinamentoPortao == HIGH)
+  {
+    servopt1(90);
+    servopt2(90);
+  }
+  if (AcionemntoPortao == LOW)
+  {
+    servopt1(0);
+    servopt2(180);
+  }
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+void blink(int var, int time) {
+
+  if (millis() - tempo < time) {
+    digitalWrite(var, LOW);
+  }
+  if ((millis() - tempo > time) && (millis() - tempo < (2 * time))) {
+    digitalWrite(var, HIGH);
+  }
+  if (millis() - tempo > (2 * time)) {
+    digitalWrite(var, LOW);
+    tempo = millis();
+  }
 }
